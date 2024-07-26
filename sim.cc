0d144880cad6df08907ff551d134b21318e0c6ca
Sharod Roy <sharodroy7@gmail.com>
2024-07-26 10:51:11 -0600

    mac files

--- sim.cc
+++ sim.cc
@@ -20,21 +20,29 @@ int main(int argc, char** argv)
 
     runManager->Initialize();
 
-    G4UIExecutive *ui = new G4UIExecutive(argc, argv);
+    G4UIExecutive *ui = 0;
+    
+    if(argc == 1 )
+    {
+        ui = new G4UIExecutive(argc, argv);
+    }
 
     G4VisManager *visManager = new G4VisExecutive();
     visManager->Initialize();
 
     G4UImanager *UImanager = G4UImanager::GetUIpointer();
 
-    UImanager->ApplyCommand("/vis/open OGL");
-    UImanager->ApplyCommand("/vis/viewer/set/viewpointVector 1 1 1");
-    UImanager->ApplyCommand("/vis/drawVolume");
-    UImanager->ApplyCommand("/vis/viewer/set/autoRefresh true");
-    UImanager->ApplyCommand("/vis/scene/add/trajectories smooth");
-    UImanager->ApplyCommand("/vis/scene/endOfEventAction accumulate");
-
-    ui->SessionStart();
+    if(ui)
+    {
+        UImanager->ApplyCommand("/control/execute vis.mac");
+        ui->SessionStart();
+    }
+    else
+    {
+        G4String command = "/control/execute ";
+        G4String fileName = argv[1];
+        UImanager->ApplyCommand(command+fileName);
+    }
 
     return 0;
 }
