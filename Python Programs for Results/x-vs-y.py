9e2546c5b30de8cff8370f64cf9f1594eaee1bbb
Sharod Roy <sharodroy7@gmail.com>
2024-07-28 23:02:35 -0600

    x vs. y visualization

--- Python Programs for Results/x-vs-y.py
+++ Python Programs for Results/x-vs-y.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load the CSV file from the specified path
 file_path = '/home/sharodroy/Downloads/Results/U/μ−/100000MeV/output.csv'
 
 # /Al/e−/
@@ -31,10 +30,8 @@ file_path = '/home/sharodroy/Downloads/Results/U/μ−/100000MeV/output.csv'
 
 data = pd.read_csv(file_path)
 
-# Strip leading and trailing spaces from column names
 data.columns = data.columns.str.strip()
 
-# Extract the relevant columns for plotting
 x = data['PositionX']
 y = data['PositionY']
 
@@ -46,8 +43,6 @@ plt.xlabel('PositionX')
 plt.ylabel('PositionY')
 plt.grid(True)
 
-# Save the plot as an image file
 plt.savefig('/home/sharodroy/Downloads/Results/U/μ−/100000MeV/PositionX_vs_PositionY.png')
 
-# Show the plot
 plt.show()
